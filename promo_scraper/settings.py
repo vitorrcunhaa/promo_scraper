e5254e65d6610e361cf6722806c2aaf884d9f976
Vitor Cunha <vitorrcunhaa@gmail.com>
2020-08-22 22:19:59 -0300

    Configure for heroku deploy
    - Also add useful links

--- promo_scraper/settings.py
+++ promo_scraper/settings.py
@@ -9,6 +9,7 @@ https://docs.djangoproject.com/en/3.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.1/ref/settings/
 """
+import django_heroku
 import os
 from pathlib import Path
 
@@ -131,3 +132,5 @@ LOGOUT_REDIRECT_URL = 'home'
 
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
+# Activate Django-Heroku.
+django_heroku.settings(locals())
