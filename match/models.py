e375c12f6358f31d0dbf8297dd44e9171006047a
Vitor Cunha <vitorrcunhaa@gmail.com>
2023-05-16 10:51:28 -0300

    Forgot to add the changes in the model file

--- match/models.py
+++ match/models.py
@@ -8,7 +8,7 @@ class Match(models.Model):
 
     name = models.CharField(max_length=128)
     price = models.DecimalField(max_digits=10, decimal_places=2)
-    link = models.URLField()
+    link = models.URLField(max_length=350)
     coupon = models.CharField(max_length=128, blank=True, null=True)
     date_match_found = models.DateTimeField(auto_now_add=True)
     user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
