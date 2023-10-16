53f1147d3a1b3a9c0b18ccf7b219bba5d6662659
Vitor Cunha <vitorrcunhaa@gmail.com>
2023-10-16 10:38:56 -0300

    Comment static dir

--- promo_scraper/settings.py
+++ promo_scraper/settings.py
@@ -123,9 +123,9 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 
 # Extra places for collectstatic to find static files.
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
-)
+# STATICFILES_DIRS = (
+#     os.path.join(BASE_DIR, 'static'),
+# )
 
 LOGIN_REDIRECT_URL = '/core'
 LOGOUT_REDIRECT_URL = 'home'
