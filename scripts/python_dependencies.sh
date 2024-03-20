a23aa6e9be024027df66425e6eb5a0e3b9aa9f06
Vitor Cunha <vitorrcunhaa@gmail.com>
2024-03-20 15:03:00 -0300

    Fix paths

--- scripts/python_dependencies.sh
+++ scripts/python_dependencies.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 cd /home/ubuntu/promo_scraper/ || { echo "Failure, could not cd into /home/ubuntu/promo_scraper/"; exit 1; }
-virtualenv /home/ubuntu/promo_scraper/env
+virtualenv env
 echo env created successfully
-source /env/bin/activate
+source env/bin/activate
 echo env activated successfully
-pip install -r /home/ubuntu/promo_scraper/requirements.txt
+pip install -r requirements.txt
 echo requirements installed successfully
